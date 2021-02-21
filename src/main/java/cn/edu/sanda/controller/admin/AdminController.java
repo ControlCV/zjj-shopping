224dfe9f6fbeccdd11fe1750bdd70709ad32aaae
ControlCV <1916168788@qq.com>
2021-02-22 00:30:11 +0800

    项目初始化

--- src/main/java/cn/edu/sanda/controller/admin/AdminController.java
+++ src/main/java/cn/edu/sanda/controller/admin/AdminController.java
@@ -1,14 +1,18 @@
 package cn.edu.sanda.controller.admin;
 
+import cn.edu.sanda.entity.Admin;
 import cn.edu.sanda.entity.Menu;
-import cn.edu.sanda.service.menu.MenuService;
+import cn.edu.sanda.service.AdminService;
+import cn.edu.sanda.service.MenuService;
 import cn.edu.sanda.utils.TreeNode;
 import cn.edu.sanda.utils.TreeNodeBulider;
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONArray;
+import cn.edu.sanda.vo.AdminRiderManageRequestVo;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.thymeleaf.util.ObjectUtils;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
@@ -21,18 +25,21 @@ import java.util.List;
 @Controller
 @RequestMapping(value = {"admin","/"})
 public class AdminController {
-@Autowired
-private MenuService menuService;
+    @Autowired
+    private MenuService menuService;
 
-//登录首页
-@GetMapping(value = {"/login","/"})
-public String adminLogin() {
+    @Autowired
+    private AdminService adminService;
+
+    //登录首页
+    @GetMapping(value = {"/login","/"})
+    public String adminLogin() {
         return "/admin/login";
-        }
+    }
 
-//控制面板
-@RequestMapping("/index")
-public String adminIndex(HttpServletRequest request) {
+    //控制面板
+    @RequestMapping("/index")
+    public String adminIndex(HttpServletRequest request) {
         //查询用户的账号权限，暂时没做
 
         //查询菜单表
@@ -41,13 +48,13 @@ public String adminIndex(HttpServletRequest request) {
         List<TreeNode> nodes = new ArrayList<>();
         //将list组装为权限树
         for (Menu menu: menuList) {
-        Integer id = menu.getId();
-        Integer pid = menu.getPid();
-        String title = menu.getTitle();
-        String icon = menu.getIcon();
-        String href = menu.getHref();
-        Boolean spread = menu.getSpread().equals("0")?true:false;
-        nodes.add(new TreeNode(id,pid,title,icon,href,spread));
+            Integer id = menu.getId();
+            Integer pid = menu.getPid();
+            String title = menu.getTitle();
+            String icon = menu.getIcon();
+            String href = menu.getHref();
+            Boolean spread = menu.getSpread().equals("0")?true:false;
+            nodes.add(new TreeNode(id,pid,title,icon,href,spread));
         }
 
         List<TreeNode> builder = TreeNodeBulider.builder(nodes, 1);
@@ -58,10 +65,46 @@ public String adminIndex(HttpServletRequest request) {
         request.getSession().setAttribute("menuList",builder);
 
         return "/admin/index";
+    }
+
+    //骑手管理界面
+    @RequestMapping("/riderManage")
+    public String adminRiderManage(HttpServletRequest request) {
+        return  "/admin/riderManage";
+    }
+
+
+    //查询骑手
+    @ResponseBody
+    @RequestMapping("/riderManageNumber")
+    public List<Admin> adminRiderManageNumber(AdminRiderManageRequestVo riderManageRequestVo) {
+
+        System.out.println(riderManageRequestVo);
+        QueryWrapper<Admin> adminQueryWrapper = new QueryWrapper<>();
+        adminQueryWrapper.eq("admin_type","1");
+
+        if(riderManageRequestVo.getAdminAccout()!=null){
+            adminQueryWrapper.like("admin_accout",riderManageRequestVo.getAdminAccout());
+        }
+        if(riderManageRequestVo.getAdminName()!=null){
+            adminQueryWrapper.like("admin_name",riderManageRequestVo.getAdminName());
+        }
+        if(riderManageRequestVo.getAdminPhone()!=null){
+            adminQueryWrapper.like("admin_phone",riderManageRequestVo.getAdminPhone());
+        }
+        if(riderManageRequestVo.getAdminGender()!=null){
+            adminQueryWrapper.like("admin_gender",riderManageRequestVo.getAdminGender());
+        }
+        if(riderManageRequestVo.getAdminAddress()!=null){
+            adminQueryWrapper.like("admin_address",riderManageRequestVo.getAdminAddress());
         }
 
+        List<Admin> list = adminService.list(adminQueryWrapper);
+
+        return  list;
+    }
 
 
 
 
-        }
+}
--- src/main/java/cn/edu/sanda/entity/Admin.java
+++ src/main/java/cn/edu/sanda/entity/Admin.java
@@ -1,5 +1,8 @@
 package cn.edu.sanda.entity;
 
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
 import java.util.Date;
 import java.io.Serializable;
 
@@ -7,106 +10,51 @@ import java.io.Serializable;
  * 用户信息表(Admin)实体类
  *
  * @author makejava
- * @since 2020-11-05 23:41:17
+ * @since 2021-02-21 17:37:43
  */
+@Data
+@TableName("admin")
 public class Admin implements Serializable {
-    private static final long serialVersionUID = -46768851485575115L;
+    private static final long serialVersionUID = -18731846383917294L;
     /**
     * 自增id
     */
     private Integer id;
     /**
+    * 用户账户
+    */
+    private String adminAccout;
+    /**
     * 用户真实姓名
     */
     private String adminName;
     /**
-    * 用户账户
+    * 用户手机号
     */
-    private String accoutNumber;
-    /**
-    * 用户昵称
-    */
-    private String nickName;
+    private String adminPhone;
     /**
     * 用户密码
     */
     private String adminPwd;
     /**
-    * 用户头像，默认头像
+    * 用户性别
     */
-    private Integer personalAvatar;
+    private Integer adminGender;
     /**
     * 用户地址
     */
-    private String personalAddress;
+    private String adminAddress;
     /**
     * 用户生日
     */
-    private Date personalBirthday;
-
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getAdminName() {
-        return adminName;
-    }
-
-    public void setAdminName(String adminName) {
-        this.adminName = adminName;
-    }
-
-    public String getAccoutNumber() {
-        return accoutNumber;
-    }
-
-    public void setAccoutNumber(String accoutNumber) {
-        this.accoutNumber = accoutNumber;
-    }
-
-    public String getNickName() {
-        return nickName;
-    }
-
-    public void setNickName(String nickName) {
-        this.nickName = nickName;
-    }
-
-    public String getAdminPwd() {
-        return adminPwd;
-    }
-
-    public void setAdminPwd(String adminPwd) {
-        this.adminPwd = adminPwd;
-    }
-
-    public Integer getPersonalAvatar() {
-        return personalAvatar;
-    }
-
-    public void setPersonalAvatar(Integer personalAvatar) {
-        this.personalAvatar = personalAvatar;
-    }
-
-    public String getPersonalAddress() {
-        return personalAddress;
-    }
-
-    public void setPersonalAddress(String personalAddress) {
-        this.personalAddress = personalAddress;
-    }
-
-    public Date getPersonalBirthday() {
-        return personalBirthday;
-    }
-
-    public void setPersonalBirthday(Date personalBirthday) {
-        this.personalBirthday = personalBirthday;
-    }
+    private Date adminBirthday;
+    /**
+     * 用户类型
+     */
+    private String adminType;
+    /**
+     *用户头像
+     */
+    private String adminPhoto;
 
 }
